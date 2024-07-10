e36554bfcc24578805a121a67f62d8c2a458e8af
Fábio Miguel <111529458+FabioMMs@users.noreply.github.com>
2024-07-10 11:38:51 -0300

    Atualizações do Robo.v (Lógica Reset + Lógica Entulho)
    Código da Máquina de Estados do Robô foi atualizado com a lógica de Reset, robô ao entrar em StandBy não pode realizar nenhuma ação até o próximo pulso de reset, e com a lógica de aguardo de 3 pulsos no estado retirando entulho, cada entulho necessita de, no mínimo, três pulsos para concluir tal ação. Dessa forma, após 3 pulsos é que será novamente verificado o estado dos sensores.

--- Robo.v
+++ Robo.v
@@ -1,173 +1,217 @@
-module Robo (clock, reset, head, left, avancar, girar, under, rubble);
+module Robo (clock, reset, head, left, avancar, girar, under, rubble, recolher_entulho);
 // Inputs, Outputs, Registradores e Parâmetros
 input clock, reset, head, left, under, rubble;
 output reg avancar, girar, recolher_entulho;
-reg [1:0] EstadoAtual, EstadoFuturo;
-reg[1:0] contador = 1'b1;
+reg [2:0] EstadoAtual, EstadoFuturo; // Corrigido para 3 bits, já que temos 5 estados
+reg [1:0] contador;
+reg flag_Stop;
 
 parameter StandBy = 3'b000,
-	  Avancando = 3'b001,
-	  Rotacionando = 3'b010,
-	  Ret_Entulho = 3'b011,
-	  Giros = 3'b100;
-	  
-
+          Avancando = 3'b001,
+          Rotacionando = 3'b010,
+          Ret_Entulho = 3'b011,
+          Giros = 3'b100;
+          
 // Definição FSM
-always @*
-avancar = 1'b0;
-girar = 1'b0;
-recolher_entulho = 1'b0;
-
+always @* 
 begin
-// Switch (Case):
-case (EstadoAtual)
-	StandBy: case({head, left, under, rubble})
-			4'b1??1: 
-				begin
-				EstadoFuturo = StandBy;
-				end
-			4'b0??0:
-				begin
-				EstadoFuturo = Avancando;
-				avancar = 1'b1;
-				end
-			4'b10?0:
-				begin
-				EstadoFuturo = Rotacionando;
-				girar = 1'b1;
-				end
-			4'b0??1:
-				begin
-				EstadoFuturo = Ret_Entulho;
-				recolher_entulho = 1'b1;
-				end
-			4'b11?0:
-				begin
-				EstadoFuturo = Giros;
-				girar = 1'b1;
-				end
-		endcase
+    avancar = 1'b0;
+    girar = 1'b0;
+    recolher_entulho = 1'b0;
 
-	Avancando: case({head, left, under, rubble})
-			4'1??1: 
-				begin
-				EstadoFuturo = StandBy;
-				end
-			4'??1?:
-				begin
-				EstadoFuturo = StandBy;
-				end
-			4'b0100:
-				begin
-				EstadoFuturo = Avancando;
-				avancar = 1'b1;
-				end
-			4'b?000:
-				begin
-				EstadoFuturo = Rotacionando;
-				girar = 1'b1;
-				end
-			4'b0?01:
-				begin
-				EstadoFuturo = Ret_Entulho;
-				recolher_entulho = 1'b1;
-				end
-			4'b1100:
-				begin
-				EstadoFuturo = Giros;
-				girar = 1'b1;
-				end
-			endcase	
-
-	Rotacionando: case({head, left, under, rubble})
-			4'1??1: 
-				begin
-				EstadoFuturo = StandBy;
-			4'b0??0:
-				begin
-				EstadoFuturo = Avancando;
-				avancar = 1'b1;
-				end
-			4'b1??0:
-				begin
-				EstadoFuturo = Rotacionando;
-				girar = 1'b1;
-				end
-			4'b0??1:
-				begin
-				EstadoFuturo = Ret_Entulho;
-				recolher_entulho = 1'b1;
-				end
-			endcase	
-
-	Ret_Entulho: case({head, left, under, rubble})
-			4'1???: 
-				begin
-				EstadoFuturo = StandBy;
-				end
-			4'b0??0:
-				begin
-				EstadoFuturo = Avancando;
-				avancar = 1'b1;
-				end
-			4'b0??1:
-				begin
-				EstadoFuturo = Ret_Entulho;
-				recolher_entulho = 1'b1;
-				end
-			endcase	
-
-	Giros: case({head, left, under, rubble})
-			4'1??1: 
-				begin
-				EstadoFuturo = StandBy;
-				end
-			4'00?0:
-				begin
-				EstadoFuturo = Avancando;
-				girar = 1'b1;
-				end
-			4'b01?0:
-				begin
-				EstadoFuturo = Avancando;
-				avancar = 1'b1;
-				end
-			4'b11?0:
-				begin
-				EstadoFuturo = Rotacionando;
-				girar = 1'b1;
-				end
-			4'b0??1:
-				begin
-				EstadoFuturo = Ret_Entulho;
-				recolher_entulho = 1'b1;
-				end
-			4'b10?0:
-				begin
-				EstadoFuturo = Giros;
-				girar = 1'b1;
-				end
-			endcase	
-
-	
-	default:
-		begin
-		EstadoFuturo = StandBy;
-		end
-endcase
-end
-
-// Att de Estado e Reset
-always @(negedge clock or negedge reset)
-if(reset)
-	EstadoAtual <= StandBy;
-
-else
+    case (EstadoAtual)
+        StandBy: 
 	begin
-	if (contador)
-		EstadoAtual <= EstadoFuturo;
-	contador <= ~contador;
-	end
+            case ({head, left, under, rubble})
+                4'b1??1: EstadoFuturo = StandBy;
+
+                4'b0??0: 
+		begin
+                    EstadoFuturo = Avancando;
+                    avancar = 1'b1;
+                end
+
+                4'b10?0: 
+		begin
+                    EstadoFuturo = Rotacionando;
+                    girar = 1'b1;
+                end
+
+                4'b0??1: 
+		begin
+                    EstadoFuturo = Ret_Entulho;
+                    recolher_entulho = 1'b1;
+                end
+
+                4'b11?0: 
+		begin
+                    EstadoFuturo = Giros;
+                    girar = 1'b1;
+                end
+
+                default: EstadoFuturo = StandBy;
+            endcase
+        end
+        
+        Avancando: 
+	begin
+            case ({head, left, under, rubble})
+                4'b1??1: EstadoFuturo = StandBy;
+
+                4'b??1?: EstadoFuturo = StandBy;
+
+                4'b0100: 
+		begin
+                    EstadoFuturo = Avancando;
+                    avancar = 1'b1;
+                end
+
+                4'b?000: 
+		begin
+                    EstadoFuturo = Rotacionando;
+                    girar = 1'b1;
+                end
+
+                4'b0?01: 
+		begin
+                    EstadoFuturo = Ret_Entulho;
+                    recolher_entulho = 1'b1;
+                end
+
+                4'b1100: 
+		begin
+                    EstadoFuturo = Giros;
+                    girar = 1'b1;
+                end
+
+                default: EstadoFuturo = StandBy;
+            endcase
+        end
+
+        Rotacionando:
+	 begin
+            case ({head, left, under, rubble})
+                4'b1??1: EstadoFuturo = StandBy;
+
+                4'b0??0: 
+		begin
+                    EstadoFuturo = Avancando;
+                    avancar = 1'b1;
+                end
+
+                4'b1??0: 
+		begin
+                    EstadoFuturo = Rotacionando;
+                    girar = 1'b1;
+                end
+
+                4'b0??1: 
+		begin
+                    EstadoFuturo = Ret_Entulho;
+                    recolher_entulho = 1'b1;
+                end
+
+                default: EstadoFuturo = StandBy;
+            endcase
+        end
+        
+        Ret_Entulho: 
+	begin
+            case ({head, left, under, rubble})
+                4'b1???: EstadoFuturo = StandBy;
+
+                4'b0??0: 
+		begin
+                    EstadoFuturo = Avancando;
+                    avancar = 1'b1;
+                end
+
+                4'b0??1: 
+		begin
+                    EstadoFuturo = Ret_Entulho;
+                    recolher_entulho = 1'b1;
+                end
+
+                default: EstadoFuturo = StandBy;
+            endcase
+        end
+        
+        Giros: 
+	begin
+            case ({head, left, under, rubble})
+                4'b1??1: EstadoFuturo = StandBy;
+
+                4'b00?0: 
+		begin
+                    EstadoFuturo = Avancando;
+                    girar = 1'b1;
+                end
+
+                4'b01?0: 
+		begin
+                    EstadoFuturo = Avancando;
+                    avancar = 1'b1;
+                end
+
+                4'b11?0: 
+		begin
+                    EstadoFuturo = Rotacionando;
+                    girar = 1'b1;
+                end
+
+                4'b0??1: 
+		begin
+                    EstadoFuturo = Ret_Entulho;
+                    recolher_entulho = 1'b1;
+                end
+
+                4'b10?0: 
+		begin
+                    EstadoFuturo = Giros;
+                    girar = 1'b1;
+                end
+
+                default: EstadoFuturo = StandBy;
+            endcase
+        end
+        
+        default: EstadoFuturo = StandBy;
+    endcase
 end
 
+// Atualização de Estado e Reset
+always @(negedge clock or posedge reset) 
+    begin
+    if (reset)
+	begin
+        EstadoAtual <= StandBy;
+	contador <= 2'b00;			// Contador de Pulsos (Ret_Entulho) setado para 0
+	flag_Stop <= 1'b1;			// Flag de botão Reset setado para 1
+	end
+
+    else 
+	begin
+	if (EstadoAtual == Ret_Entulho && contador != 2'b11)
+		begin
+		contador <= contador + 1;
+		end
+
+      	else
+		begin
+		if (EstadoAtual == StandBy && flag_Stop == 1'b1)
+			begin
+			EstadoAtual <= EstadoFuturo;
+			flag_Stop <= 1'b0;
+			end
+		
+		else
+			begin
+        		EstadoAtual <= EstadoFuturo;
+        		if (EstadoAtual != Ret_Entulho)
+				contador <= 2'b00;
+			end
+		end
+	end
+   end
 endmodule
+

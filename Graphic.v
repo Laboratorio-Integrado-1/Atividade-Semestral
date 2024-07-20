5c574e42655a692a94dee3948aee2238f45386dd
LadraoDeHavaiana <tluigimm@gmail.com>
2024-07-19 21:46:19 -0300

    conexão módulo gráfico e módulo VGA

--- Graphic.v
+++ Graphic.v
@@ -1,12 +1,14 @@
-module Graphic(clock, mapa)
-input clock
-input mapa [3:0] Mapa [0:10][0:19];
+module Graphic(clock, reset, map, vga_R, vga_G, vga_B);
 
- // Configuração do mapa 
- // 0: Caminho Livre -	Célula onde o robô pode se mover livremente.
- // 1: Parede - Obstáculo que impede o movimento do robô.
- // 2: Célula Preta - Célula que indica o inicio ou final de uma tubulação.
- // 3: Entulho Leve - Entulho que requer 3 ciclos para ser removido.
- // 4: Entulho Médio - Entulho que requer 6 ciclos para ser removido.
- // 5: Entulho Pesado - Entulho que requer 9 ciclos para ser removido.
+input [3:0] Mapa [0:10][0:19]; // Atualizacao para 10 linhas e 20 colunas
+output vga_R, vga_G, vga_B;
+
+wire vga_R, vga_G, vga_B;
+
+for (int i=0; i<10; i++) begin
+	for (int j=0; j<20; j++) begin
+		VGA DUV(clock, reset, Mapa[i][j], i*30, j*30, vga_R, vga_G, vga_B);
+	end
+end
+
 endmodule
---
+++ ip_inst.v
@@ -0,0 +1,6 @@
+ip	ip_inst (
+	.areset ( areset_sig ),
+	.inclk0 ( inclk0_sig ),
+	.c0 ( c0_sig ),
+	.locked ( locked_sig )
+	);
